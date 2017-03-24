5f0a6def033a5c475a225bac3fa41c3d58621c31
Dmitry Bikulov <dmitry@bikulov.org>
2017-03-24 11:13:22 +0300

    * add parsing arguments * use json as input source for template * use named fields in template

--- htmlgenerator-1.py
+++ htmlgenerator-1.py
@@ -1,16 +1,37 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
 """
 Created on Thu Mar 23 21:44:22 2017
 
 @author: Student
 """
-mStr=input()
-mStr=mStr.split()
 
-with open('source-1.txt') as finpt:
-    a = finpt.read()
-b = a.format(*mStr)
+import argparse
+import json
 
-with open('htmlGen-1.html', 'w') as fout:
-    fout.write(b)
+def Generate(inputTemplate, content, outputHtml):
+    with open(inputTemplate) as finpt:
+        template = finpt.read()
+        renderedHtml = template.format(**content)
+
+    with open(outputHtml, 'w') as fout:
+        fout.write(renderedHtml)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        'Meme generator',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument('-i', '--input', default='source-1.txt', help='Input template')
+    parser.add_argument('-c', '--content', default='source-1.json', help='Content for template')
+    parser.add_argument('-o', '--output', default='htmlGen-1.html', help='Output template')
+
+    args = parser.parse_args()
+
+    with open(args.content) as fin:
+        content = json.load(fin)
+
+    Generate(args.input, content, args.output)
 
